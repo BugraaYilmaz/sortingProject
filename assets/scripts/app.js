3cce6f7c02c3cb810dafacad5694484dce68dc12
Buğra Yılmaz <131853576+BugraaYilmaz@users.noreply.github.com>
2023-10-16 15:53:23 +0300

    d2.2

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -20,31 +20,35 @@ function deleteAllItems(){
   for(const elem of barDom){
     elem.remove();
   }
+  barDom.length=0;
 }
 
 function generateItems(){
   for(let i=0;i<arrLen;i++){
     const item=document.createElement("div");
-    item.style.backgroundColor="blue";
+    item.style.backgroundColor="#FAF0E6";
     item.style.height=heightGenerator()+"px";
     item.style.width=(900/(arrLen*1.2))+"px";
     item.style.padding="0px";
     item.style.margin=(200/(arrLen*1.2))+"px";
     item.style.display="inline-block"
-    item.className="eachValue"
     barDom.push(item);
     sArea.appendChild(item);
   }
 }
 function heightGenerator(){
   const ranNum=Math.random();
-  return(ranNum*500);
+  return(ranNum*400);
 }
 
 function generateBars(){
   getArrLen();
   deleteAllItems();
   generateItems();
+}
+
+function bubbleSort(){
+  
 }
 
 
